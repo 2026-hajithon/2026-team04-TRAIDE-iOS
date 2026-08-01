3cdfce65d5c57d0c067a5da23c3230987974fe57
jiwookim1202 <godblessjiwoo@gmail.com>
2026-08-01 20:01:50 +0900

    [fix] 폴더 이동

---
+++ TRAIDE/TRAIDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRAIDEApp.swift
+//  TRAIDE
+//
+//  Created by 김지우 on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TRAIDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
